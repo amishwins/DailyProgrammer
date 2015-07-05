33587f0c8ca2a5bc164fda0528717407aff7eeff
Amish <amish.gala@gmail.com>
2015-07-05 01:57:05 -0400

    Switched the implementation to using BigInteger. Long fails after 19 digits. Seems to be fine. Run the test 'theLogic' for the challenge inputs and outputs

--- src/main/java/easy218/SuperPal.java
+++ src/main/java/easy218/SuperPal.java
@@ -1,13 +1,102 @@
 package easy218;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SuperPal {
+	List<BigInteger> bigIntsToTest;
+	
+	public SuperPal() {
+		bigIntsToTest = new ArrayList<>();
+	}
+	
+	public void register(String number) {
+		bigIntsToTest.add(new BigInteger(number));
+	}
+	
+	// looks like longs are no good. Have to switch to big integer!
+	public void run() {
+		for(BigInteger bigInt: bigIntsToTest) {
+			int count = 0;
+			BigInteger palindrome = new BigInteger(bigInt.toString());
+			boolean error = false;
+			
+			while(!isBigIntegerAPal(palindrome)) {
+				palindrome = palindrome.add(flipBigInteger(palindrome));
 
-	// Given a number, is it a palindrome? 
-	boolean isNumberAPal(int i) {
-		String numberAsString = Integer.toString(i);
+				if (count > 100) {
+					error = true;
+					break;
+				}
+				count++;
+			}
+			
+			if (!error) {
+				System.out.println(bigInt + " gets palindromic after " + count + " steps: " + palindrome);
+			} else {
+				System.out.println(bigInt + " results in too many steps");
+			}
+		}
+	}	
+	
+	private List<Character> reverse(List<Character> characterList) {
+		List<Character> result = new ArrayList<>();
+		for (int i = characterList.size() - 1; i >= 0; i--) {
+			result.add(characterList.get(i));
+		}
+		
+		return result;
+	}
+
+	private List<Character> getCharacterList(String left) {
+		List<Character> result = new ArrayList<>();
+		for (int i = 0; i < left.length(); i++) {
+			result.add(left.charAt(i));
+		}
+		
+		return result;
+	}
+
+	public boolean isBigIntegerAPal(BigInteger i) {
+		String numberAsString = i.toString();
+		if (numberAsString.length() == 1) return true;
+		
+		StringSplitter ss = new StringSplitter(numberAsString);
+		String left = ss.getLeftSide();
+		String right = ss.getRightSide();
+		
+		// Two optimizations (necessary?)
+		if (left.equals(right)) {
+			return true;
+		} else if (left.length() == 1) {
+			return false;			
+		} else {
+			List<Character> leftList = getCharacterList(left);
+			List<Character> rightList = reverse(getCharacterList(right));
+			
+			return (leftList.equals(rightList));
+		}	}
+
+	public BigInteger flipBigInteger(BigInteger bigInteger) {
+		if (isBigIntegerAPal(bigInteger)) return bigInteger;
+		
+		String number = bigInteger.toString();
+		StringBuilder sb = new StringBuilder();
+		for(int index = number.length() - 1; index >= 0; index--) {
+			sb.append(number.charAt(index));
+		}
+
+		return new BigInteger(sb.toString());
+	}
+	
+	
+	// ****************************************************************************
+	// Keeping the implementation using long here for now just to remember
+	// that this implementation fails after the palindrome grows beyond 19 digits
+	// ****************************************************************************
+	boolean isNumberAPal(long l) {
+		String numberAsString = Long.toString(l);
 		if (numberAsString.length() == 1) return true;
 		
 		StringSplitter ss = new StringSplitter(numberAsString);
@@ -26,25 +115,22 @@ public class SuperPal {
 			return (leftList.equals(rightList));
 		}
 	}
+
 	
-
-	private List<Character> reverse(List<Character> characterList) {
-		List<Character> result = new ArrayList<>();
-		for (int i = characterList.size() - 1; i >= 0; i--) {
-			result.add(characterList.get(i));
-		}
+	// ****************************************************************************
+	// Keeping the implementation using long here for now just to remember
+	// that this implementation fails after the palindrome grows beyond 19 digits
+	// ****************************************************************************
+	public long flipNumber(long l) {
+		if (isNumberAPal(l)) return l;
 		
-		return result;
-	}
-
-
-	private List<Character> getCharacterList(String left) {
-		List<Character> result = new ArrayList<>();
-		for (int i = 0; i < left.length(); i++) {
-			result.add(left.charAt(i));
+		String number = Long.toString(l);
+		StringBuilder sb = new StringBuilder();
+		for(int index = number.length() - 1; index >= 0; index--) {
+			sb.append(number.charAt(index));
 		}
-		
-		return result;
+
+		return Long.parseLong(sb.toString());
 	}
 
 }
--- src/test/java/easy218/SuperPalTest.java
+++ src/test/java/easy218/SuperPalTest.java
@@ -4,11 +4,44 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 public class SuperPalTest {
 	SuperPal cut = new SuperPal();
 
+	@Test 
+	public void theLogic() {
+		cut.register("24");
+		cut.register("28");
+		cut.register("11");
+		cut.register("68");
+		cut.register("123");
+		cut.register("286");
+		cut.register("196196871");
+		cut.run();
+	}
+	
+	@Test
+	public void uhOhNeedBigLetItSnow() {
+		assertFalse(cut.isBigIntegerAPal(new BigInteger("196196871")));
+		assertFalse(cut.isBigIntegerAPal(new BigInteger("19619687111123123123123123123123123231232")));
+		assertTrue(cut.isBigIntegerAPal(new BigInteger("4478555400006996000045558744")));
+	}
+	
+	@Test
+	public void uhOhNeedFlipBigInteger() {
+		assertEquals(new BigInteger("21"), cut.flipBigInteger(new BigInteger("12")));
+		assertEquals(new BigInteger("11"), cut.flipBigInteger(new BigInteger("11")));
+		assertEquals(new BigInteger("11"), cut.flipBigInteger(new BigInteger("110")));
+	}
+
+	
+	// ****************************************************************************
+	// Keeping the tests here using long for now just to remember
+	// that this implementation fails after the palindrome grows beyond 19 digits
+	// ****************************************************************************
 	@Test
 	public void theseNumbersArePalindromes() {
 		assertTrue(cut.isNumberAPal(6));
@@ -27,5 +60,13 @@ public class SuperPalTest {
 		assertFalse(cut.isNumberAPal(12324));
 		assertFalse(cut.isNumberAPal(123326));
 	}
+	
+	@Test
+	public void flipNumber() {
+		assertEquals(21, cut.flipNumber(12));
+		assertEquals(11, cut.flipNumber(11));
+		assertEquals(11, cut.flipNumber(110));
+	}
+
 	
 }
